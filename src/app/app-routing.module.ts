b40be4915dbb6cd9875b9157e998fb92449de452
Andre Andrino <andrebaumgratz@gmail.com>
2023-12-17 22:07:02 -0300

    novas edicoes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { JogosComponent } from './views/jogos/jogos.component';
+import { HomeComponent } from './views/home/home.component';
+import { HeaderComponent } from './views/header/header.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'jogos',
+    component: JogosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,34 @@ import { BrowserModule } from '@angular/platform-browser';
 /* Angula Material  */
 
 import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NovoComponent } from './novo/novo.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './views/header/header.component';
+import { JogosComponent } from './views/jogos/jogos.component';
+import { HomeComponent } from './views/home/home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NovoComponent
+    NovoComponent,
+    HeaderComponent,
+    JogosComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
